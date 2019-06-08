911829a5a81b902ed5ddc2c5869c87de9f809675
Lukiian <lukian37@gmail.com>
2019-06-08 15:06:37 +0300

    Add project files.

---
+++ DZ5WEBAPI/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.ADO;
+using Microsoft.AspNetCore.Mvc;
+using Models.Models;
+
+namespace DZ5WEBAPI.Controllers
+{
+    public class CoursesController : ControllerBase
+    {
+        private readonly Repository repository;
+
+        public CoursesController(Repository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet("Courses")]
+        public List<Course> GetCourses()
+        {
+            var result = repository.GetAllCourses();
+            return result;
+        }
+
+    }
+}
---
+++ DZ5WEBAPI/Controllers/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.ADO;
+using Microsoft.AspNetCore.Mvc;
+using Models.Models;
+
+namespace DZ5WEBAPI.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly Repository repository;
+
+        public StudentsController(Repository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpPost("Students")]
+        public Student CreateStudent([FromBody]Student student)
+        {
+            repository.CreateStudent(student);
+            return student;
+        }
+
+        [HttpGet("Students/{id}")]
+        public ActionResult<Student> GetStudent([FromRoute]int id)
+        {
+            var result = repository.GetAllStudents().FirstOrDefault(s => s.Id == id);
+            if (result == null)
+            {
+                return new NotFoundResult();
+            }
+            return result;
+        }
+
+        [HttpPut("Students")]
+        public Student UpdateStudent([FromBody]Student student)
+        {
+            repository.UpdateStudent(student);
+            var result = repository.GetAllStudents().FirstOrDefault(s => s.Id == student.Id);
+            result = student;
+            return result;
+        }
+
+        [HttpDelete("Students/{id}")]
+        public ActionResult DeleteStudent(int id)
+        {
+            repository.DeleteStudent(id);
+            return new OkResult();
+        }
+    }
+}
